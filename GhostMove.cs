4f46922e6b4f71e7c9fccd6113493023a3a93314
Chengxin-Wu <112346517+Chengxin-Wu@users.noreply.github.com>
2022-11-01 23:44:46 -0400

    Add files via upload

---
+++ GhostMove.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GhostMove : MonoBehaviour 
+{
+	public GameObject[] WayPointsGos;
+	public float speed = 0.2f;
+	private List<Vector3> wayPoints = new List<Vector3> ();
+	private int index = 0;
+	private Vector3 startPos;
+
+	private void Start()
+	{
+		startPos = transform.position + new Vector3 (0, 3, 0);
+		LoadPath(WayPointsGos[gameManager.Instance.usingIndex[GetComponent<SpriteRenderer>().sortingOrder - 2]]);
+	}
+
+	private void LoadPath(GameObject go)
+	{
+		wayPoints.Clear ();
+		foreach (Transform t in go.transform) {
+			wayPoints.Add (t.position);
+		}
+		wayPoints.Insert (0, startPos);
+		wayPoints.Add (startPos);
+	}
+
+	private void FixedUpdate()
+	{
+		if (transform.position != wayPoints [index]) 
+		{
+			Vector2 temp = Vector2.MoveTowards (transform.position, wayPoints [index], speed);
+			GetComponent<Rigidbody2D> ().MovePosition (temp);
+		} 
+		else 
+		{
+			index++;
+			if (index >= wayPoints.Count) 
+			{
+				index = 0;
+				LoadPath (WayPointsGos [Random.Range (0, WayPointsGos.Length)]);
+			}
+		}
+
+		Vector2 dir = wayPoints [index] - transform.position;
+		GetComponent<Animator> ().SetFloat ("DirX", dir.x);
+		GetComponent<Animator> ().SetFloat ("DirY", dir.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.name == "Pacman") 
+		{
+			if (gameManager.Instance.isSuperPacman) 
+			{
+				transform.position = startPos - new Vector3(0,3,0);
+				index = 0;
+				gameManager.Instance.Score += 500;
+			} 
+			else 
+			{
+				collision.gameObject.SetActive (false);
+				gameManager.Instance.GamePanel.SetActive (false);
+				Instantiate (gameManager.Instance.GameOverPrefab);
+				Invoke ("ReStart", 3f);
+			}
+		}
+	}
+
+	private void ReStart()
+	{
+		SceneManager.LoadScene (0);
+	}
+}
---
+++ PacmanMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour 
+{
+	public float speed = 0.35F;
+	private Vector2 dest = Vector2.zero;
+
+	private void Start()
+	{
+		dest = transform.position;
+	}
+
+	private void FixedUpdate()
+	{
+		Vector2 temp = Vector2.MoveTowards (transform.position, dest, speed);
+		GetComponent<Rigidbody2D>().MovePosition (temp);
+		if ((Vector2)transform.position == dest) 
+		{
+			if ((Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W) ) && Vaild(Vector2.up)) 
+			{
+				dest = (Vector2)transform.position + Vector2.up;
+			}
+			if ((Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S) ) && Vaild(Vector2.down)) 
+			{
+				dest = (Vector2)transform.position + Vector2.down;
+			}
+			if ((Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A) ) && Vaild(Vector2.left)) 
+			{
+				dest = (Vector2)transform.position + Vector2.left;
+			}
+			if ((Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D) ) && Vaild(Vector2.right)) 
+			{
+				dest = (Vector2)transform.position + Vector2.right;
+			}
+		}
+	}
+
+	private bool Vaild(Vector2 dir)
+	{
+		Vector2 pos = transform.position;
+		RaycastHit2D hit = Physics2D.Linecast (pos + dir, pos);
+		return (hit.collider == GetComponent<Collider2D> ());
+	}
+}
